e1ab8b641bf7e8fd662558d918a11caaeb022048
msalvalaglio <matteo.salvalaglio@gmail.com>
2021-10-23 09:19:58 +0100

    MD example: added comments and general tidy up

--- _build/jupyter_execute/MD_example.py
+++ _build/jupyter_execute/MD_example.py
@@ -37,15 +37,19 @@ M=np.array([[0, 1, 0, 0, 0, 0, 0, 0],
            [0, 0, 0, 1, 0, 1, 0, 0],
            [0, 0, 0, 0, 1, 0, 1, 0],
            [0, 0, 0, 0, 0, 1, 0, 1],
-           [0, 0, 0, 0, 0, 0, 1, 0]])
+           [0, 0, 0, 0, 0, 0, 1, 0]]); 
 k=k*M;
 
-# velocities
-v0=0.5*(np.random.rand(2,8)-0.5);
+# Random initial velocities
+v0=0.25*(np.random.rand(2,8)-0.5);
 
 ## Integration and Visualization
 
+### Initialise the system
+
+%%capture
 %matplotlib inline
+
 # Setup figure for plotting the trajectory
 
 figure, axes = plt.subplots(figsize=(5, 5))
@@ -79,52 +83,54 @@ time[1]=time[0]+dt;
 POT=np.zeros(np.shape(time));
 KIN=np.zeros(np.shape(time));
 
-# Start integrating
-for timestep in np.arange(1,nsteps):
-    c=next(color)
-    timestep=int(timestep)
+### Compute Trajectory
+
+# Compute trajectory
+for timestep in np.arange(1,nsteps): #Cycle over timesteps
+    c=next(color)                    #Update color for static representation
+    timestep=int(timestep)           #Make sure timestep is an integer
     
     # Initialise force vectors
     fx=np.zeros(np.size(x0));  
     fy=np.zeros(np.size(x0)); 
     
-    # Compute distances and the interparticle forces
+    # Compute distances and the interparticle forces for every pair of particles
     for i in np.arange(0,np.size(x0)):
         for j in np.arange(i+1,np.size(x0)):
             
-            r=np.sqrt((x[i]-x[j])*(x[i]-x[j])+(y[i]-y[j])*(y[i]-y[j]));     
+            r=np.sqrt((x[i]-x[j])*(x[i]-x[j])+(y[i]-y[j])*(y[i]-y[j])); #Distance    
                
-            cx=-(k[i,j]*(r-req)-2*HS/(np.power(r,3)))*((x[i]-x[j]))/r;
-            cy=-(k[i,j]*(r-req)-2*HS/(np.power(r,3)))*((y[i]-y[j]))/r;  
+            cx=-(k[i,j]*(r-req)-2*HS/(np.power(r,3)))*((x[i]-x[j]))/r;  #Pairwise force in x
+            cy=-(k[i,j]*(r-req)-2*HS/(np.power(r,3)))*((y[i]-y[j]))/r;  #Pairwise force in y
                 
-            fx[i]=fx[i]+cx;      
-            fx[j]=fx[j]-cx;
-            
-            fy[i]=fy[i]+cy;
-            fy[j]=fy[j]-cy;
+            fx[i]=fx[i]+cx;      #update total x-component of the force on particle i
+            fx[j]=fx[j]-cx;      #update total x-component of the force on particle j 
+
+            fy[i]=fy[i]+cy;      #update total y-component of the force on particle i
+            fy[j]=fy[j]-cy;      #update total y-component of the force on particle j 
            
        
     #Verlet integration
     for i in np.arange(0,np.size(x0)):
-        r=np.sqrt(np.power(x[i],2)+np.power(y[i],2));
-        xnew[i]=2*x[i]-xp[i]+(dt*dt)*fx[i]/m[i];
-        ynew[i]=2*y[i]-yp[i]+(dt*dt)*fy[i]/m[i];
+        xnew[i]=2*x[i]-xp[i]+(dt*dt)*fx[i]/m[i]; # new position (x-component)
+        ynew[i]=2*y[i]-yp[i]+(dt*dt)*fy[i]/m[i]; # new position (y-component)
     
     # Compute velocity     
     vx=(xnew-xp)/2/dt;
     vy=(ynew-yp)/2/dt;
     v=np.sqrt(np.power(vx,2)+np.power(vy,2)); 
     
-    # Assign positions
+    # Reassign positions
     xp=x; yp=y; x=xnew-np.mean(xnew); y=ynew-np.mean(ynew);
     
-    
+    # Static representation of the trajectory
     line, = axes.plot(x,y,marker='o',color=c,markersize=10,linestyle='-')
     
-    ## Store trajectory for animation
+    ## Store trajectory for animation 
     xx[timestep]=x;
     yy[timestep]=y;
 
+Visa
 
 %%capture
 %matplotlib inline
@@ -153,3 +159,4 @@ anim = animation.FuncAnimation(fig, animate, init_func=init,
                                frames=np.size(np.arange(1,nsteps)), interval=50);
 
 HTML(anim.to_html5_video())
+
