1db9e3067254f4722ca881f6907f13f418dd2c90
msalvalaglio <matteo.salvalaglio@gmail.com>
2023-10-30 16:41:37 +0000

    W3 exercise

--- _build/jupyter_execute/Week3_Exercise_Harmonic.py
+++ _build/jupyter_execute/Week3_Exercise_Harmonic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Week 2 Exercise: The harmonic oscillator. 
+# # Week 3 Exercise: The harmonic oscillator. 
 # 
 # Given a 1D Harmonic oscillator with characteristic frequency $\omega=5$Hz, mass of 20, starting at the initial position of 1, with an initial momentum of 1. 
 # 
@@ -10,10 +10,6 @@
 # - How would you represent the motion of the Harmonic oscillator in Phase Space? 
 # 
 # - What is the most probable state for the Harmonic oscillator? 
-# 
-# 
-# 
-# 
 
 # In[8]:
 
