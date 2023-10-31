1845a3f2e791b6252846bbce007894adfef703a0
msalvalaglio <matteo.salvalaglio@gmail.com>
2023-10-31 18:07:20 +0000

    updated MD HO with example Ideal Gas

--- _build/jupyter_execute/Week3_Exercise_Harmonic.py
+++ _build/jupyter_execute/Week3_Exercise_Harmonic.py
@@ -10,36 +10,3 @@
 # - How would you represent the motion of the Harmonic oscillator in Phase Space? 
 # 
 # - What is the most probable state for the Harmonic oscillator? 
-
-# In[8]:
-
-
-get_ipython().run_line_magic('matplotlib', 'notebook')
-get_ipython().run_line_magic('matplotlib', 'inline')
-import matplotlib.pyplot as plt 
-import numpy as np
-
-## Parameters
-m=1; #mass
-k=2; # Harmonic Constant
-v0=1; 
-x0=1; 
-
-om=np.sqrt(k/m)
-
-t=np.linspace(0,10,100)
-
-print(t)
-
-x=x0*np.cos(np.prod(om,t))+v0*sin(om*t)
-
-print(x)
-
-plt.plot(t,x); plt.show()
-
-
-# In[ ]:
-
-
-
-
